29b4a5e915bdaa6d8bab220931f59c35db93bbb1
Aditya Pant <pantanupam05@gmail.com>
2025-01-02 12:54:44 +0530

    structure

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.post("/user/signup", (req, res) => {
+	res.json({
+		message: "signup endpoint",
+	});
+});
+app.post("/user/signin", (req, res) => {
+	res.json({
+		message: "signup endpoint",
+	});
+});
+app.post("/user/purchases", (req, res) => {
+	res.json({
+		message: "signup endpoint",
+	});
+});
+app.post("/courses/purchases", (req, res) => {
+	res.json({
+		message: "signup endpoint",
+	});
+});
+app.post("/courses", (req, res) => {
+	res.json({
+		message: "signup endpoint",
+	});
+});
+app.post("/user/signup", (req, res) => {
+	res.json({
+		message: "signup endpoint",
+	});
+});
